c797320830fde7934860130bd2ca435a28a520ed
ram3ez <rameez12m@gmail.com>
2026-01-12 13:18:56 +0530

    removed success

--- src/app/auth/AuthClient.tsx
+++ src/app/auth/AuthClient.tsx
@@ -65,8 +65,8 @@ const oAuthSignup = (
   const s = account.createOAuth2Session({
     provider:
       authProvider === "google" ? OAuthProvider.Google : OAuthProvider.Github,
-    success: `${origin}/auth/oauth/callback`,
-    failure: `${origin}/auth?error=oauth_failed`,
+    /* success: `${origin}/auth/oauth/callback`,
+    failure: `${origin}/auth?error=oauth_failed`, */
   });
 
   console.log("this is a test: " + s);
