f75ef795764f85759218707a7a884b38fb8964bd
ram3ez <rameez12m@gmail.com>
2026-01-26 14:36:47 +0530

    revert

--- src/app/auth/oauth/callback/route.ts
+++ src/app/auth/oauth/callback/route.ts
@@ -1,5 +1,4 @@
 // app/auth/oauth/callback/route.ts
-"use client";
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { checkUserProfile, setSessionCookie } from "@/lib/actions/auth";
