f27cc119aebe8cb69c295756a11b5bd207163c1d
Athul-Kesav <me.nd.mself@gmail.com>
2026-02-06 17:23:28 +0530

    feat: complete accommodation flow

---
+++ src/app/(main)/residence-placeholder/page.tsx
@@ -0,0 +1,10 @@
+import { ComingSoon } from "@/components/ui/coming-soon";
+
+export default function ResidencePage() {
+  return (
+    <ComingSoon
+      title="Accommodation"
+      message="Details regarding accommodation and stay will be available soon. Please check back later!"
+    />
+  );
+}
